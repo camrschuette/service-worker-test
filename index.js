65fa631a26cf6994154884b5deb16d4b62db5054
Cameron Schuette <cschuette@criticalmix.com>
2017-04-20 14:27:16 -0400

    more registration code

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', function() {
-        navigator.serviceWorker.register('sw.js')
-        .then(function (registration) {
-            console.log('ServiceWorker registration successful with scope: ', registration.scope);
-        }, function (err) {
-            console.log('ServiceWorker registration failed: ', err);
-        });
+        // Register a Service Worker.
+		navigator.serviceWorker.register('service-worker.js')
+		.then(function(registration) {
+		  // Use the PushManager to get the user's subscription to the push service.
+		  return registration.pushManager.getSubscription()
+		  .then(function(subscription) {
+				// If a subscription was found, return it.
+				if (subscription) {
+				  return subscription;
+				}
+
+				// Otherwise, subscribe the user (userVisibleOnly allows to specify that we don't plan to
+				// send notifications that don't have a visible effect for the user).
+				return registration.pushManager.subscribe({ userVisibleOnly: true });
+			});
+		}).then(function(subscription) {
+			endpoint = subscription.endpoint;
+		});
     });
 }
