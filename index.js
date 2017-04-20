a3a02426f885e5da49abbfd272a59dbddc718492
Cameron Schuette <cschuette@criticalmix.com>
2017-04-20 14:28:55 -0400

    Merge branch 'gh-pages'

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', function() {
         // Register a Service Worker.
-		navigator.serviceWorker.register('service-worker.js')
+		navigator.serviceWorker.register('sw.js')
 		.then(function(registration) {
 		  // Use the PushManager to get the user's subscription to the push service.
 		  return registration.pushManager.getSubscription()
