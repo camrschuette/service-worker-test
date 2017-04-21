8cb03dd8148b6e535b6ec380be51dbc4c8fed82f
Cameron Schuette <cschuette@criticalmix.com>
2017-04-21 11:43:54 -0400

    add more listeners to sw

--- sw.js
+++ sw.js
@@ -1,5 +1,15 @@
 'use strict';
 
+self.addEventListener('install', function (event) {
+    self.skipWaiting();
+
+    console.log('Installed', event);
+});
+
+self.addEventListener('activate', function (event) {
+    console.log('Activated', event);
+});
+
 // Register event listener for the 'push' event.
 self.addEventListener('push', function(event) {
   // Keep the service worker alive until the notification is created.
